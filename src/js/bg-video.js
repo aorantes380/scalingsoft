78f8cce1f12426e86eb97f968bb5f74048a0fab3
Angelo Orantes <aorantesp@gmail.com>
2021-05-07 18:47:11 -0600

    new changes

---
+++ src/js/bg-video.js
@@ -0,0 +1,61 @@
+
+const tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+const bgVideoID = document.querySelector('.js-background-video').getAttribute('data-video');
+
+
+const playerOptions = {
+  autoplay: 1,
+  mute: 1,
+  autohide: 1, 
+  modestbranding: 1, 
+  rel: 0, 
+  showinfo: 0, 
+  controls: 0, 
+  disablekb: 1, 
+  enablejsapi: 1, 
+  iv_load_policy: 3,
+  loop: 1,
+  playlist: bgVideoID,
+  
+}
+
+const videoOverlay = document.querySelector('.js-video-overlay');
+
+let ytPlayer;
+function onYouTubeIframeAPIReady() {
+  ytPlayer = new YT.Player('yt-player', {
+    width: '1280',
+    height: '720',
+    videoId: bgVideoID,
+    playerVars: playerOptions,
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+
+  const videoDuration = event.target.getDuration();
+  
+  setInterval(function (){
+    const videoCurrentTime = event.target.getCurrentTime();
+    const timeDifference = videoDuration - videoCurrentTime;
+    
+    if (2 > timeDifference > 0) {
+      event.target.seekTo(0);
+    }
+  }, 1000);
+}
+
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING) {
+    videoOverlay.classList.add('header__video-overlay--fadeOut');
+  }
+}
